76f33b7e9251f54af5722fbfafedef13e7ac7328
pr4tikshaaa <pratiksha0220@gmail.com>
2026-03-12 21:21:45 -0700

    Add PriceComparator class with comparison methods
    Implemented price comparison logic with ascending order option.

--- PriceComparator.java
+++ PriceComparator.java
@@ -5,5 +5,38 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
     // TODO complete class
+    private boolean ascending = true;
+    
+    public PriceComparator() {
+
+        ascending = true;
+    }
+
+    public PriceComparator(boolean asc) {
+        ascending = asc;
+    }
+
+    public int compare(TradeOrder order1, TradeOrder order2) {
+        if (order1.isMarket() && order2.isMarket()) {
+            return 0;
+        }
+
+        if (order1.isMarket() && order2.isLimit()) {
+            return -1;
+        }
+
+        if (order1.isLimit() && order2.isMarket()) {
+            return 1;
+        }
+
+        int cents1 = (int) order1.getPrice() * 100;
+        int cents2 = (int) order2.getPrice() * 100;
+
+        if (ascending) {
+            return cents1 - cents2;
+        } else {
+            return cents2 - cents1;
+        }
+    }
 
 }
