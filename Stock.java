8d48168364df23c48adfbb3c85121f3e51971e33
wafflio <73447309+wafflioz@users.noreply.github.com>
2026-03-22 13:27:40 -0700

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -17,7 +17,92 @@ public class Stock
 
     // TODO complete class
 
-    
+    public Stock(String symbol, String name, double price) {
+        stockSymbol = symbol;
+        companyName = name;
+        loPrice = price;
+        hiPrice = price;
+        lastPrice = price;
+        volume = 0;
+        sellOrders = new PriorityQueue<TradeOrder>(new PriceComparator(true));
+        buyOrders = new PriorityQueue<TradeOrder>(new PriceComparator(false));
+    }
+
+    public String getQuote() {
+        StringBuilder quote = new StringBuilder();
+        quote.append(companyName).append(" (").append(stockSymbol).append(")\n");
+        quote.append("Price: ").append(money.format(lastPrice)).append("  hi: ").append(money.format(hiPrice)).append("  lo: ").append(money.format(loPrice)).append("  vol: ").append(volume).append("\n");
+        String askStr = "none";
+        if (!sellOrders.isEmpty()) {
+            TradeOrder topSell = sellOrders.peek();
+            if (topSell.isLimit()) {
+                askStr = money.format(topSell.getPrice()) + " size: " + topSell.getShares();
+            } else {
+                askStr = "market size: " + topSell.getShares();
+            }
+        }
+        String bidStr = "none";
+        if (!buyOrders.isEmpty()) {
+            TradeOrder topBuy = buyOrders.peek();
+            if (topBuy.isLimit()) {
+                bidStr = money.format(topBuy.getPrice()) + " size: " + topBuy.getShares();
+            } else {
+                bidStr = "market size: " + topBuy.getShares();
+            }
+        }
+        quote.append("Ask: ").append(askStr).append("  Bid: ").append(bidStr);
+        return quote.toString();
+    }
+
+    public void placeOrder(TradeOrder order) {
+        if (order.isBuy()) {
+            buyOrders.add(order);
+        } else {
+            sellOrders.add(order);
+        }
+        String orderMsg = "New order:  " + (order.isBuy() ? "Buy" : "Sell") + " " + stockSymbol + " (" + companyName + ")\n" + order.getShares() + " shares at " + (order.isMarket() ? "market" : money.format(order.getPrice()));
+        order.getTrader().receiveMessage(orderMsg);
+        executeOrders();
+    }
+
+    protected void executeOrders() {
+        while (!buyOrders.isEmpty() && !sellOrders.isEmpty()) {
+            TradeOrder buy = buyOrders.peek();
+            TradeOrder sell = sellOrders.peek();
+            double execPrice = 0;
+            boolean canExec = false;
+            if (buy.isLimit() && sell.isLimit()) {
+                if (buy.getPrice() >= sell.getPrice()) {
+                    execPrice = sell.getPrice();
+                    canExec = true;
+                }
+            } else if (buy.isMarket() && sell.isMarket()) {
+                execPrice = lastPrice;
+                canExec = true;
+            } else {
+                if (buy.isLimit()) {
+                    execPrice = buy.getPrice();
+                } else {
+                    execPrice = sell.getPrice();
+                }
+                canExec = true;
+            }
+            if (!canExec) break;
+            int sharesToTrade = Math.min(buy.getShares(), sell.getShares());
+            buy.subtractShares(sharesToTrade);
+            sell.subtractShares(sharesToTrade);
+            if (buy.getShares() == 0) buyOrders.poll();
+            if (sell.getShares() == 0) sellOrders.poll();
+            loPrice = Math.min(loPrice, execPrice);
+            hiPrice = Math.max(hiPrice, execPrice);
+            lastPrice = execPrice;
+            volume += sharesToTrade;
+            String buyMsg = "You bought: " + sharesToTrade + " " + stockSymbol + " at " + money.format(execPrice) + " amt " + money.format(execPrice * sharesToTrade);
+            String sellMsg = "You sold: " + sharesToTrade + " " + stockSymbol + " at " + money.format(execPrice) + " amt " + money.format(execPrice * sharesToTrade);
+            buy.getTrader().receiveMessage(buyMsg);
+            sell.getTrader().receiveMessage(sellMsg);
+        }
+    }
     //
     // The following are for test purposes only
     //
